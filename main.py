40dabda08d49a813754136a6496bc0e1adce8f15
Gerardo Salas Montoya <ore395@hotmail.com>
2024-04-18 14:30:52 -0600

    adding socket.io webrtc config

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, Response, request, redirect, session, abort, g
 import cv2
 from flask_misaka import Misaka
+from flask_socketio import SocketIO, emit
 from src.supabase import (
     supabase,
     user_context_processor,
@@ -12,11 +13,37 @@ from src.auth import auth
 
 app = Flask(__name__, template_folder="./templates", static_folder="./static")
 Misaka(app)
+socketio = SocketIO(app)
 
 # Set the secret key to some random bytes. Keep this really secret!
 app.secret_key = b"c8af64a6a0672678800db3c5a3a8d179f386e083f559518f2528202a4b7de8f8"
 app.context_processor(user_context_processor)
 app.register_blueprint(auth)
+
+
+@socketio.on('connect')
+def handle_connect():
+    print(f"A user connected to the server: {request.sid}")
+
+    # Emit a 'join' event to all clients except the current one
+    emit('event', {'type': 'join', 'from': request.sid, 'role': request.args.get('role')}, broadcast=True, include_self=False)
+
+@socketio.on('event')
+def handle_event(evt):
+    print(f"A signaling event was sent: {evt.get('type')}")
+
+    # Emit the event to a specific client (evt['to'])
+    target_client = evt.get('to')
+    if target_client:
+        emit('event', evt, room=target_client)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print(f"A user is leaving: {request.sid}")
+
+    # Emit a 'bye' event to all clients except the current one
+    emit('event', {'type': 'bye', 'from': request.sid}, broadcast=True, include_self=False)
+
 
 def generate_frames():
     if not camera.isOpened():
@@ -38,13 +65,9 @@ def generate_frames():
 
     camera.release()
 
-
-
 @app.teardown_appcontext
 def close_supabase(e=None):
     g.pop("supabase", None)
-
-
 
 @app.route('/')
 def home():
@@ -56,10 +79,15 @@ def stream():
     print(session)
     return render_template('streaming.html')
 
+@app.route('/watcher')
+def watcher():
+    session = supabase.auth.refresh_session
+    print(session)
+    return render_template('watcher.html')
+
 @app.route('/video')
 def video():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
 
 @app.route("/dashboard")
 @login_required
@@ -69,9 +97,6 @@ def dashboard():
     profile = get_profile_by_user()
     return render_template("dashboard.html", profile=profile)
 
-
-
-
 if __name__ == "__main__":
     camera = cv2.VideoCapture(0)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
---
+++ static/track-utils.js
@@ -0,0 +1,138 @@
+function createProcessedTrack({ track, transform }) {
+    const trackProcessor = new MediaStreamTrackProcessor({ track });
+    const trackGenerator = new MediaStreamTrackGenerator({ kind: track.kind });
+  
+    const transformer = new TransformStream({ transform });
+  
+    trackProcessor.readable
+      .pipeThrough(transformer)
+      .pipeTo(trackGenerator.writable);
+  
+    return trackGenerator;
+  }
+  
+  function cleanStream() {
+    return function transform(frame, controller) {
+      controller.enqueue(frame);
+    }
+  }
+  
+  function showText({
+    text,
+    txtInitialX,
+    txtColor = 'white',
+    txtFontSize = '48px',
+    txtFont = 'serif',
+    textSpeed = 2,
+    bgColor = '#08b9a6',
+    bgPadding = 10,
+    position = 'top'
+  }) {
+    const canvas = new OffscreenCanvas(1, 1);
+    const ctx = canvas.getContext('2d');
+    const intTxtFontSize = parseInt(txtFontSize);
+  
+    let x = txtInitialX;
+  
+    return function transform(frame, controller) {
+      const width = frame.displayWidth;
+      const height = frame.displayHeight;
+      canvas.width = width;
+      canvas.height = height;
+  
+      const bgHeight = intTxtFontSize + bgPadding;
+      const bgPositionY = position === 'bottom'
+        ? height - (intTxtFontSize + bgPadding + 5)
+        : 5;
+      const txtPositionY = position === 'bottom'
+        ? height - (Math.floor(bgPadding / 2) + 10)
+        : 5 + intTxtFontSize;
+  
+      ctx.clearRect(0, 0, width, height);
+      ctx.drawImage(frame, 0, 0, width, height);
+      ctx.font = txtFontSize + ' ' + txtFont;
+      ctx.fillStyle = bgColor;
+      ctx.fillRect(0, bgPositionY, width, bgHeight)
+      ctx.fillStyle = txtColor;
+      ctx.fillText(text, x, txtPositionY);
+  
+      x -= textSpeed;
+      if (x <= (0 - 100 - text.length * 20)) {
+        x = width
+      }
+  
+      const newFrame = new VideoFrame(canvas, { timestamp: frame.timestamp });
+  
+      frame.close();
+      controller.enqueue(newFrame);
+    }
+  }
+  
+  function showImage({ 
+    image,
+    imgPositionX = 10,
+    imgPositionY = 10,
+    imgWidth = 450,
+    imgHeight = 320
+  }) {
+    const canvas = new OffscreenCanvas(1, 1);
+    const ctx = canvas.getContext('2d');
+    const img = new Image();
+    img.src = URL.createObjectURL(image);
+  
+    return function transform(frame, controller) {
+      const width = frame.displayWidth;
+      const height = frame.displayHeight;
+      canvas.width = width;
+      canvas.height = height;
+  
+      ctx.clearRect(0, 0, width, height);
+      ctx.drawImage(frame, 0, 0, width, height);
+      ctx.drawImage(img, imgPositionX, imgPositionY, imgWidth, imgHeight);
+  
+      const timestamp = frame.timestamp;
+      frame.close();
+  
+      const newFrame = new VideoFrame(canvas, { timestamp });
+      controller.enqueue(newFrame);
+    }
+  }
+  
+  function showQr({
+    text,
+    qrWidth = 256,
+    qrHeight = 256,
+    colorDark = '#000000',
+    colorLight = '#FFFFFF',
+    positionX = 10,
+    positionY = 10
+  }) {
+    const canvas = new OffscreenCanvas(1, 2);
+    const ctx = canvas.getContext('2d');
+    const qrDiv = document.createElement('div');
+    
+    new QRCode(qrDiv, {
+      text,
+      width: qrWidth,
+      height: qrHeight,
+      colorDark,
+      colorLight
+    });
+  
+    return function transform(frame, controller) {
+      const width = frame.displayWidth;
+      const height = frame.displayHeight;
+      canvas.width = width;
+      canvas.height = height;
+  
+      ctx.clearRect(0, 0, width, height);
+      ctx.drawImage(frame, 0, 0, width, height);
+      ctx.drawImage(qrDiv.querySelector('canvas'), positionX, positionY, qrWidth, qrHeight);
+  
+      const timestamp = frame.timestamp;
+      frame.close();
+  
+      const newFrame = new VideoFrame(canvas, { timestamp });
+      controller.enqueue(newFrame);
+    }
+  }
